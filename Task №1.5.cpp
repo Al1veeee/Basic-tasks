d7115f6d9243897db7e2eeaaccf2d9ef1119ec33
Alimkhanov Deni <145188607+Al1veeee@users.noreply.github.com>
2023-12-27 18:23:02 +0300

    Task №1.5.cpp

--- Task №1.5.cpp
+++ Task №1.5.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
 	string day, curtains, lamp;
-	cout << "Is it light outside?(Enter \"yes\" or \"no\"):\n"; cin >> day;
-	cout << "Are the curtains drawn?\n"; cin >> curtains;
-	cout << "Is the lamp on?\n"; cin >> lamp;
+	cout << "Is it light outside?(Enter \"yes\" or \"no\"):\n"; cin >> day; //Светло ли наружи?
+	cout << "Are the curtains drawn?\n"; cin >> curtains; //Шторы раскрыты?
+	cout << "Is the lamp on?\n"; cin >> lamp; //Лампа включена
 
 	if (((day == "yes") && (curtains == "yes")) || (lamp == "yes"))
 	{
